efe228518c1c6ead4ff9f15ef6ab0a6d81c08a70
unknown <mitesharora96@gmail.com>
2020-11-20 23:40:31 +0530

    Basic Component Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
+import { InterestComponent } from './interest/interest.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectComponent,
+    InterestComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
